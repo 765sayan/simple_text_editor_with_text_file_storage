e704649023c4f5a38ecfbdc787664752ed8a6a65
765sayan <sayanchakraborty765@gmail.com>
2023-08-23 00:47:54 +0530

    refactor: :art: removed imports in code

--- backend/src/routes/v1/authRoutes.ts
+++ backend/src/routes/v1/authRoutes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { getUserInfoController, loginController, registerController } from "../../controllers/authControllers";
 import { auth } from "../../middlewares/authMiddlewares";
 
