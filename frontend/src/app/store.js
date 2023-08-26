34200e4966d42edae3b229c0e7c4fec956401383
765sayan <sayanchakraborty765@gmail.com>
2023-08-26 14:13:23 +0530

    fix: :bug: Fixed import related bugs

--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import demoSlice from "../features/demo/demoSlice";
-
-export const store = configureStore({
-  reducer: {
-    demo: demoSlice,
-  },
-});
